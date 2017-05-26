b3ed1a670ec99214478eca6feb9e5b76d04b583f
nikhileshRayapureddy <naga.nikhil65@gmail.com>
2017-05-26 15:07:44 +0530

    c

---
+++ CapillaryFramework.framework/Headers/CapillaryFramework.h
@@ -0,0 +1,21 @@
+//
+//  CapillaryFramework.h
+//  CapillaryFramework
+//
+//  Created by Nikhilesh on 05/05/17.
+//  Copyright © 2017 Capillary Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class BusinessLayer;
+@class BaseBL;
+@class AuthSignature;
+//! Project version number for CapillaryFramework.
+FOUNDATION_EXPORT double CapillaryFrameworkVersionNumber;
+
+//! Project version string for CapillaryFramework.
+FOUNDATION_EXPORT const unsigned char CapillaryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CapillaryFramework/PublicHeader.h>
+
+
